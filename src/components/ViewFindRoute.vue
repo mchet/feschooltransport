afb2ebe814f52e4d0e63c4125c1920ac367ea437
mchetver <maxim.chetverikov@gmail.com>
2019-04-05 16:42:52 +0300

    created project

---
+++ src/components/ViewFindRoute.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ViewFindRoute"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ViewTransportList.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ViewTransportList"
+    }
+</script>
+
+<style scoped>
+
+</style>
